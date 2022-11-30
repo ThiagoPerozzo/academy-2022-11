9cd3b20e9afd257cb9abcf389cb5f547aac1ef5b
Thiago Perozzo Cardoso <thiagoperozzo@gmail.com>
2022-11-30 23:27:43 +0000

    adicionadas novas stg e criadas todas as dimensoes (dim)

---
+++ models/marts/comercial/dim_funcionarios.sql
@@ -0,0 +1,41 @@
+with
+    funcionarios as (
+        select *
+        from {{ ref('stg_erp__funcionarios') }}
+    )
+
+--self join com ela mesmo por conta dos ids dos funcionarios e dos gerentes,
+--nao da para fazer isso direto na staging pois é é algo que está contido na propria tabela
+    , self_joined as (
+        select
+            funcionarios.id_funcionario
+            , funcionarios.id_gerente
+            , funcionarios.nome_do_funcionario
+            , funcionarios.sobrenome_do_funcionario
+            , funcionarios.nome_completo_funcionario
+            , funcionarios.titulo_de_cortesia
+            , funcionarios.data_de_nascimento
+            , funcionarios.cargo
+            , funcionarios.data_da_contratacao
+            , gerentes.nome_completo_funcionario as gerente
+            , funcionarios.endereco
+            , funcionarios.cep
+            , funcionarios.cidade
+            , funcionarios.regiao
+            , funcionarios.pais
+            , funcionarios.telefone
+            , funcionarios.observacoes
+        from funcionarios
+        left join funcionarios as gerentes on
+            funcionarios.id_gerente = gerentes.id_funcionario
+    )
+
+    , transformed as (
+        select
+            row_number() over (order by id_funcionario) as sk_funcionario
+            , *
+        from self_joined
+    )
+
+select *
+from transformed
